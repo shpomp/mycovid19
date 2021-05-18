2232a1825c590c1371474c5f3821038e2cbfaee2
juaninicolai <juan0719@stud.kea.dk>
2021-05-18 09:51:03 +0200

    test model +  controller + html

---
+++ src/main/java/com/example/mycovid19/Controller/TestController.java
@@ -0,0 +1,43 @@
+package com.example.mycovid19.Controller;
+
+import com.example.mycovid19.Model.Test;
+import com.example.mycovid19.Repo.TestRepo;
+import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+
+  @Autowired
+  private TestRepo testRepo;
+
+  @GetMapping("/test")
+  public String test(Model model) throws SQLException {
+    model.addAttribute("tests", testRepo.fetchAll());
+    return "/test";
+  }
+
+  @PostMapping("/createTest")
+  public String addTest(Test test){
+    testRepo.addTest(test);
+    return "redirect:/test";
+  }
+
+  @PostMapping(value = "/editTest", params="delete")
+  public String cancelTest(@RequestParam("test_id") int test_id){
+    testRepo.cancelTest(test_id);
+    return "redirect:/test";
+  }
+
+  @PostMapping(value = "/editTest", params="update")
+  public String bookTest(@RequestParam("test_id") int test_id){
+    testRepo.bookTest(test_id);
+    return "redirect:/test";
+  }
+
+}
---
+++ src/main/java/com/example/mycovid19/Repo/TestRepo.java
@@ -0,0 +1,43 @@
+package com.example.mycovid19.Repo;
+
+import com.example.mycovid19.Model.Test;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestRepo {
+
+  private final static String table = "test";
+  private static JdbcTemplate jdbc;
+
+
+  public TestRepo(JdbcTemplate jdbc) {
+    TestRepo.jdbc = jdbc;
+  }
+
+  public static int addTest(Test test){
+    String sql= "INSERT INTO " + table + " VALUES (?,?,?,?)";
+    return jdbc.update(sql, null, test.getTest_date(), test.getTest_time(), test.getTest_status());
+  }
+
+  public static List<Test> fetchAll() throws SQLException {
+    String sql = " SELECT * FROM " + table; //+ " WHERE test_status = 'available' ORDER BY test_date ";
+    RowMapper<Test> rowMapper = new BeanPropertyRowMapper<>(Test.class);
+    return jdbc.query(sql, rowMapper);
+  }
+
+  public static int cancelTest(int test_id){
+    String sql = " UPDATE " + table + " SET test_status = 'available' WHERE test_id = ? ";
+    return jdbc.update(sql, test_id);
+  }
+
+  public static int bookTest(int test_id){
+    String sql = " UPDATE " + table + " SET test_status = 'booked' WHERE test_id = ? ";
+    return jdbc.update(sql, test_id);
+  }
+
+}
