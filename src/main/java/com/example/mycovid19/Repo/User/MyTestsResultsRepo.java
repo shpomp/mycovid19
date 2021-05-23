5cb5d973a6cfb5beb4a79c961a016623e761cb52
Veselin Ivanov <v3s1ez@gmail.com>
2021-05-23 16:29:35 +0200

    i

--- src/main/java/com/example/mycovid19/Repo/User/MyTestsResultsRepo.java
+++ src/main/java/com/example/mycovid19/Repo/User/MyTestsResultsRepo.java
@@ -2,16 +2,41 @@ package com.example.mycovid19.Repo.User;
 
 import com.example.mycovid19.Model.TestResult;
 import com.example.mycovid19.Service.User.MyTestsResultsService;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MyTestsResultsRepo {
 
-  private static int user_id;
+  @Autowired
+  private JdbcTemplate jdbc;
 
-  public static List<TestResult> joinTestsResults() throws SQLException {
-    return MyTestsResultsService.ResultSet(user_id);
+  public List<TestResult> joinTestsResults() throws SQLException {
+    String sql = "SELECT first_name, last_name, test_date, test_time, test_status, test_diagnosis\n"
+        + "FROM user u JOIN test t\n" + "ON u.user_id = t.user_id\n"
+        + "JOIN test_result r ON t.test_id = r.test_test_id\n" + " WHERE t.test_status='done' AND u.user_id=30";
+
+    return jdbc.query(sql, new RowMapper<TestResult>() {
+
+      @Override
+      public TestResult mapRow(ResultSet rs, int rowNum) throws SQLException {
+        String firstName = rs.getString("first_name");
+        String lastName = rs.getString("last_name");
+        String testDate = rs.getString("test_date");
+        String testTime = rs.getString("test_time");
+        String testStatus = rs.getString("test_status");
+        String testDiagnosis = rs.getString("test_diagnosis");
+
+        return new TestResult(firstName, lastName, testDate, testTime, testStatus, testDiagnosis);
+      }
+
+    });
   }
 }
--- src/main/java/com/example/mycovid19/Service/User/MyTestsResultsService.java
+++ src/main/java/com/example/mycovid19/Service/User/MyTestsResultsService.java
@@ -7,57 +7,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class MyTestsResultsService {
+  // Don't think we need this...
 
-
-    static ArrayList<TestResult> list = new ArrayList<>();
-    static int user_id;
-
-    public static ArrayList<TestResult> ResultSet(int user_id) {
-
-      //DB setup
-      String username = "mycovid19";
-      String password  = "Constanza1.";
-      String database = "mycovid19";
-      String url = "jdbc:mysql://den1.mysql6.gear.host:3306/";
-
-      try(Connection conn = DriverManager.getConnection(url + database, username, password)){
-        if (!conn.isClosed()){
-          String sql =
-              "SELECT first_name, last_name, test_date, test_time, test_status, test_diagnosis\n"
-               + "FROM user u JOIN test t\n"
-               + "ON u.user_id = t.user_id\n"
-               + "JOIN test_result r ON t.test_id = r.test_test_id\n"
-               + " WHERE t.test_status='done' AND u.user_id=30";
-
-          PreparedStatement ps = conn.prepareStatement(sql);
-          java.sql.ResultSet resultSet = ps.executeQuery();
-
-          while (resultSet.next()) {
-            String firstName = resultSet.getString("first_name");
-            String lastName = resultSet.getString("last_name");
-            String testDate = resultSet.getString("test_date");
-            String testTime = resultSet.getString("test_time");
-            String testStatus = resultSet.getString("test_status");
-            String testDiagnosis = resultSet.getString("test_diagnosis");
-
-            TestResult testResult = new TestResult(
-                firstName,
-                lastName,
-                testDate,
-                testTime,
-                testStatus,
-                testDiagnosis
-            );
-            list.add(testResult);
-          }
-
-        } else {
-          System.out.println("No connection");
-        }
-
-      } catch (Exception e){
-        System.out.println("Error " + e.getMessage());
-      }
-      return list;
-    }
 }
