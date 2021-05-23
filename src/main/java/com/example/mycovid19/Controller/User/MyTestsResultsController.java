27de5db56be9369113c7d5ed22f2d59efa43939f
Veselin Ivanov <v3s1ez@gmail.com>
2021-05-23 18:01:36 +0200

    fix: autowire myTestsResultsRepo

--- src/main/java/com/example/mycovid19/Controller/User/MyTestsResultsController.java
+++ src/main/java/com/example/mycovid19/Controller/User/MyTestsResultsController.java
@@ -2,6 +2,8 @@ package com.example.mycovid19.Controller.User;
 
 import com.example.mycovid19.Repo.User.MyTestsResultsRepo;
 import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyTestsResultsController {
 
+  @Autowired
   private MyTestsResultsRepo myTestsResultsRepo;
 
   @GetMapping("/user/my_results")
