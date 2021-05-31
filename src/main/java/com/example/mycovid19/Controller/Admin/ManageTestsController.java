96174457e7f0bb6ea11ab84cbf5b566447ca6e4c
karokea <karo7494@kea.stud.dk>
2021-05-31 20:42:55 +0200

    the secreatry

--- src/main/java/com/example/mycovid19/Controller/Admin/ManageTestsController.java
+++ src/main/java/com/example/mycovid19/Controller/Admin/ManageTestsController.java
@@ -1,9 +1,11 @@
 package com.example.mycovid19.Controller.Admin;
 
 import com.example.mycovid19.Model.Test;
+import com.example.mycovid19.Model.UserCredentials;
 import com.example.mycovid19.Repo.Admin.ManageTestsRepo;
 import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ManageTestsController {
 
+
+
   @Autowired
   private ManageTestsRepo manageTestsRepo;
 
   @GetMapping("/admin/all_tests")
-  public String testList(Model model) throws SQLException {
+  public String testList(Model model, @AuthenticationPrincipal UserCredentials userCredentials) throws SQLException {
     model.addAttribute("seeAllTests", manageTestsRepo.fetchAllTests());
+    model.addAttribute("userCred", userCredentials);
     return "/admin/all_tests";
   }
 
--- src/main/java/com/example/mycovid19/Controller/Admin/ManageTestsResultsController.java
+++ src/main/java/com/example/mycovid19/Controller/Admin/ManageTestsResultsController.java
@@ -2,9 +2,11 @@ package com.example.mycovid19.Controller.Admin;
 
 import com.example.mycovid19.Model.Test;
 import com.example.mycovid19.Model.TestResult;
+import com.example.mycovid19.Model.UserCredentials;
 import com.example.mycovid19.Repo.Admin.ManageTestsRepo;
 import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +20,9 @@ public class ManageTestsResultsController {
   private ManageTestsRepo manageTestsRepo;
 
   @GetMapping("/admin/all_tests_results")
-  public String adminResults(Model model) throws SQLException {
+  public String adminResults(Model model, @AuthenticationPrincipal UserCredentials userCredentials) throws SQLException {
     model.addAttribute("adminResults", manageTestsRepo.fetchAllTestsResults());
+    model.addAttribute("userCred", userCredentials);
     return "admin/all_tests_results";
   }
 
